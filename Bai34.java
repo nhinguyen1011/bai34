56a359e16c595e732364b45159bdfb321286c31d
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2021-06-10 09:05:17 +0700

    Update Bai34.java

--- Bai34.java
+++ Bai34.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
-public class Bai34 {
+
+
+import java.util.Scanner;
+public class Bai42 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        System.out.println(" enter N : ");
+        System.out.println(" enter n : ");
         int n= scan.nextInt();
+        System.out.println(" enter a : ");
+        int a= scan.nextInt();
+        System.out.println(" enter b : ");
+        int b= scan.nextInt();
         int sum1=0,sum2=0,sum3=0;
-        int a,b,i;
-        for (i;i<=n;i++){
+        
+        for ( int i=0;i<=n;i++){
             sum1+=i;
         }
         while (a<=n){
