0c11f0cc796412d59b50f092bf85345d9fc90e4c
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2022-04-06 11:33:57 +0700

    Update Bai34.java

--- Bai34.java
+++ Bai34.java
@@ -4,27 +4,15 @@ import java.util.Scanner;
 import java.util.Scanner;
 public class Bai42 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        System.out.println(" enter n : ");
-        int n= scan.nextInt();
-        System.out.println(" enter a : ");
-        int a= scan.nextInt();
-        System.out.println(" enter b : ");
-        int b= scan.nextInt();
-        int sum1=0,sum2=0,sum3=0;
-        
-        for ( int i=0;i<=n;i++){
-            sum1+=i;
+        float sum = 0, average = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap so : ");
+        float e = scanner.nextFloat();
+        for(int i = 1; i <= e; i++){
+            sum += i;
+            average = (e + 1)/2;
         }
-        while (a<=n){
-             sum2+=a;
-        }
-        do {
-            sum3+=b;
-        }while(b<n);
-        System.out.println(" kq1 : "+sum1);
-        System.out.println(" kq2 : "+sum2);
-        System.out.println(" kq3 : "+sum3);
+        System.out.println("Tong: " + sum) ;
+        System.out.println("Trung binh: " + average) ;
     }
 }
